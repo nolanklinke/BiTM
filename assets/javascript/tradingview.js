20d666e4b91688b97966cb200402f970fca305ec
disconaked <nolan.klinke@gmail.com>
2018-07-07 12:06:26 -0400

    put widget functionality onto seperate JS file

---
+++ assets/javascript/tradingview.js
@@ -0,0 +1,27 @@
+new TradingView.MediumWidget(
+    {
+    "container_id": "tv-medium-widget",
+    "symbols": [
+      [
+        "Bitcoin",
+        "COINBASE:BTCUSD"
+      ],
+      [
+        "Litecoin",
+        "KRAKEN:LTCUSD"
+      ],
+      [
+        "Etherium",
+        "KRAKEN:ETHUSD"
+      ]
+    ],
+    "greyText": "Quotes by",
+    "gridLineColor": "#e9e9ea",
+    "fontColor": "#83888D",
+    "underLineColor": "#dbeffb",
+    "trendLineColor": "#4bafe9",
+    "width": "1000px",
+    "height": "400px",
+    "locale": "en"
+  }
+    );
